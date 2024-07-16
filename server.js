5ffd54b1df1710235384fcf45f17215e5554082a
Yash Gangan <ganganyash160@gmail.com>
2024-07-16 21:10:02 +0530

    made into a website

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import qr from "qr-image";
+
+const app = express();
+const port = 3000;
+
+// app.set('view engine', 'html');
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile('./public/index.html');
+})
+
+app.get("/generate", (req, res) => {
+    const url = req.query.url;
+    console.log(url)
+    if (!url) {
+        return res.status(400).send('URL is required');
+      }
+    
+    const qrCode = qr.image(url, { type: 'png' });
+    res.type('png');
+    qrCode.pipe(res);
+})
+
+
+app.listen(port, () => {
+    console.log(`QR Code Generator app listening at http://localhost:${port}`);
+  });
