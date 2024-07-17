ad738947d6f73708061d902632d55e3061606f6c
Yash Gangan <ganganyash160@gmail.com>
2024-07-18 00:39:14 +0530

    test

--- api/server.js
+++ api/server.js
@@ -13,13 +13,17 @@ const __dirname = dirname(__filename);
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get("/", (req, res) => {
-  try {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  } catch (error) {
-    console.error('Error serving index.html:', error);
-    res.status(500).send('Error serving the file');
-  }
+  res.send('Welcome to QR Code Generator');
 });
+
+// app.get("/", (req, res) => {
+//   try {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+//   } catch (error) {
+//     console.error('Error serving index.html:', error);
+//     res.status(500).send('Error serving the file');
+//   }
+// });
 
 app.get("/generate", (req, res) => {
   try {
